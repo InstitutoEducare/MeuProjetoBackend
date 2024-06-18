77578572898969af11fb23a038e48c234bddf6fe
InstitutoEducare <institutoeducare.trabalho@gmail.com>
2024-06-18 17:33:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 
-mongoose.connect('mongodb+srv://victor:vitor123@cluster0.zdum0bw.mongodb.net/meu-projeto?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB', err);
-});
+// String de conexão corrigida
+const mongoUri = 'mongodb+srv://victor:victor123@<cluster-url>/meu-projeto?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB', err);
+    });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
